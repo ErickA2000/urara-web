44125265a1f0b553f4626824ae55ca9dd8153113
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-04-16 14:25:33 -0500

    despues de este commit se empieza a probar gslider-js

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  imgSlider = ["assets/img/logo_urara.png", "assets/img/1273799.png", "assets/img/1273800.png", "assets/img/1273802.png"]
+
+  ngOnInit(): void {
+  }
 
 }
