03858d835f1c96f5c6d049f5d452d3f0e652ecac
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-04-16 13:51:09 -0500

    implementado guardar modo oscuro al recargar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,22 +1,34 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   
   constructor( @Inject(DOCUMENT) private document: Document ){}
 
   isDarkMode: boolean = false;
 
+  ngOnInit(): void {
+      if(localStorage.getItem('dark-mode') == 'true'){
+        this.isDarkMode = true;
+        this.changeMode( this.isDarkMode );
+      }else{
+        this.isDarkMode = false;
+        this.changeMode( this.isDarkMode );
+      }
+  }
+
   changeMode( newValue: boolean ): void{
     if(newValue){
       this.document.body.classList.add('dark-mode');
+      localStorage.setItem('dark-mode', `${newValue}`);
     }else{
       this.document.body.classList.remove('dark-mode');
+      localStorage.setItem('dark-mode', `${newValue}`);
     }
   }
 
