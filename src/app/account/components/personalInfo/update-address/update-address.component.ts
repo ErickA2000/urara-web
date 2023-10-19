91033077c8139d7fb33760402c55919a2e7b0b3c
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-10-19 18:03:32 -0500

    realizando servicio para obtener ubicaciones de colombia

--- src/app/account/components/personalInfo/update-address/update-address.component.ts
+++ src/app/account/components/personalInfo/update-address/update-address.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DialogsService } from 'src/app/shared/services/dialogs.service';
+import { LocationsInColombiaService } from 'src/app/shared/services/locations-in-colombia.service';
 import { SharedMethodsService } from 'src/app/shared/services/shared-methods.service';
 
 @Component({
@@ -6,12 +8,62 @@ import { SharedMethodsService } from 'src/app/shared/services/shared-methods.ser
   templateUrl: './update-address.component.html',
   styleUrls: ['./update-address.component.scss']
 })
-export class UpdateAddressComponent {
+export class UpdateAddressComponent implements OnInit {
 
   public typesAddress: string[] = [ "casa", "trabajo", "otro" ];
   public typeAddressSelected: string = "";
 
-  constructor( private sharedMethodsService: SharedMethodsService ){}
+  public countries: string[] = [ "colombia" ];
+  public departments: string[] = [];
+  public citiesOrMunicipalities: string[] = [];
+
+  constructor( private sharedMethodsService: SharedMethodsService, private locationsColombia: LocationsInColombiaService, 
+    private dialogsService: DialogsService ){}
+
+  ngOnInit(): void {
+
+      
+  }
+
+  searchDepartments( country: string ){
+    this.dialogsService.openSpinner();
+    this.departments = [];
+
+    if( country.toLowerCase() === "colombia"){
+
+      this.locationsColombia.getLocation().subscribe(
+        res => {
+          
+          for( let depa of res ){
+            if( !this.departments.includes(depa.departamento) ){
+              this.departments.push(depa.departamento);
+            }
+          }
+
+          this.dialogsService.close();
+
+        }
+      )
+    }
+  }
+
+  searchCitiesOrMunicipalities( department: string ){
+    this.dialogsService.openSpinner();
+    this.citiesOrMunicipalities = [];
+
+    this.locationsColombia.getInfoDepartment( department ).subscribe(
+      res => {
+
+        for( let depa of res ){
+          if( !this.citiesOrMunicipalities.includes(depa.municipio) ){
+            this.citiesOrMunicipalities.push( depa.municipio );
+          }
+        }
+        this.dialogsService.close();
+
+      }
+    )
+  }
 
   selectTypeAddress( typeAddress: string ){
     this.typeAddressSelected = typeAddress;
--- src/app/shared/services/locations-in-colombia.service.ts
+++ src/app/shared/services/locations-in-colombia.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { Iubicaciones } from 'src/app/interfaces/shared/locationsColombia.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationsInColombiaService {
 
-  constructor() { }
+  private baseUrl = environment.URL_DATOS_COLOMBIA;
+
+  constructor( private http: HttpClient ) { }
+
+  getLocation(): Observable<Iubicaciones[]>{
+    return this.http.get<Iubicaciones[]>( this.baseUrl )
+      .pipe(
+        catchError( err => of(err) )
+      )
+  }
+
+  getInfoDepartment( departamento: string ): Observable<Iubicaciones[]>{
+    const url = `${this.baseUrl}?departamento=${departamento}`;
+
+    return this.http.get<Iubicaciones[]>( url )
+      .pipe(
+        catchError( err => of(err) )
+      )
+  }
 }
