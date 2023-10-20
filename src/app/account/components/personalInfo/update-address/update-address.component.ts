a379f3b51db9df5bd4b63a2c17e2b48ba9445702
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-10-20 15:44:16 -0500

    hecho agregar direccion

--- src/app/account/components/personalInfo/update-address/update-address.component.ts
+++ src/app/account/components/personalInfo/update-address/update-address.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/account/services/user.service';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Idireccion } from 'src/app/interfaces/auth/user.interface';
 import { DialogsService } from 'src/app/shared/services/dialogs.service';
 import { LocationsInColombiaService } from 'src/app/shared/services/locations-in-colombia.service';
 import { SharedMethodsService } from 'src/app/shared/services/shared-methods.service';
+import alertSwal from 'src/app/utils/alertSwal';
 
 @Component({
   selector: 'app-update-address',
@@ -12,17 +17,76 @@ export class UpdateAddressComponent implements OnInit {
 
   public typesAddress: string[] = [ "casa", "trabajo", "otro" ];
   public typeAddressSelected: string = "";
+  
+  public typesStreet: string[] = [ "avenida", "avenida calle", "avenida carrera", "calle", "carrera", "circular", "circunvalar", 
+  "diagonal", "manzana", "Transversal", "via" ];
+  public typeStreetSelected: string = "calle";
 
   public countries: string[] = [ "colombia" ];
   public departments: string[] = [];
   public citiesOrMunicipalities: string[] = [];
 
   constructor( private sharedMethodsService: SharedMethodsService, private locationsColombia: LocationsInColombiaService, 
-    private dialogsService: DialogsService ){}
+    private dialogsService: DialogsService, private fb: FormBuilder, private authService: AuthService, private userService: UserService ){}
+
+  addressForm = this.fb.group({
+    titulo: ['', [Validators.required]],
+    tituloOtro: ['', [ Validators.minLength(3) ]],
+    pais: ['', [ Validators.required ]],
+    departamento: ['', [ Validators.required ]],
+    ciudad: ['', [ Validators.required ]],
+    barrio: ['', [ Validators.required ]],
+    tipocalle: ['', [ Validators.required ]],
+    callenumero: ['', [ Validators.required ]],
+    numero1: ['', [ Validators.minLength(1) ]],
+    numero2: ['', [ Validators.minLength(1) ]],
+    especificacionOpcional: ['', [ Validators.minLength(5) ]]
+  });
 
   ngOnInit(): void {
 
       
+  }
+
+  save(){
+    this.dialogsService.openSpinner();
+    let tempDireccion: Idireccion = {
+      titulo: '',
+      pais: this.addressForm.get('pais')?.value!,
+      departamento: this.addressForm.get('departamento')?.value!,
+      ciudad: this.addressForm.get('ciudad')?.value!,
+      direccion: '',
+      especificacionOpcional: ''
+    };
+
+    let direcciones = this.authService.user.direcciones;
+
+    if( this.addressForm.get('titulo')?.value != "otro" ){
+      tempDireccion.titulo = this.addressForm.get('titulo')?.value!;
+    }else{
+      tempDireccion.titulo = this.addressForm.get('tituloOtro')?.value!;
+    }
+
+    tempDireccion.direccion = `Barrio ${this.addressForm.get('barrio')?.value} ${this.addressForm.get('tipocalle')?.value} ${this.addressForm.get('callenumero')?.value} #${this.addressForm.get('numero1')?.value} - ${this.addressForm.get('numero2')?.value}`;
+
+    tempDireccion.especificacionOpcional = this.addressForm.get('especificacionOpcional')?.value!;
+
+    direcciones.push(tempDireccion);
+
+    this.userService.updateData( { direcciones } ).subscribe(
+      res => {
+
+        if( res.success ){
+          this.dialogsService.close();
+          alertSwal.messageSuccess( "Agregada dirección", "" );
+
+        }else{
+          this.dialogsService.close();
+          alertSwal.messageError( res.message || "" );
+        }
+
+      }
+    )
   }
 
   searchDepartments( country: string ){
@@ -39,7 +103,7 @@ export class UpdateAddressComponent implements OnInit {
               this.departments.push(depa.departamento);
             }
           }
-
+          this.departments = this.departments.sort( (a: string, b: string) => a.localeCompare(b) );
           this.dialogsService.close();
 
         }
@@ -59,6 +123,7 @@ export class UpdateAddressComponent implements OnInit {
             this.citiesOrMunicipalities.push( depa.municipio );
           }
         }
+        this.citiesOrMunicipalities = this.citiesOrMunicipalities.sort( (a: string,b: string) => a.localeCompare(b) );
         this.dialogsService.close();
 
       }
@@ -67,7 +132,10 @@ export class UpdateAddressComponent implements OnInit {
 
   selectTypeAddress( typeAddress: string ){
     this.typeAddressSelected = typeAddress;
-    
+  }
+
+  selectTypeStreet( typeStreet: string ){
+    this.typeStreetSelected = typeStreet;
   }
 
   back(){
